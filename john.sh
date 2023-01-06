055abf9ddb9dcc9838f4a22b34f48d5e887f1918
the44exploit <121959269+the44exploit@users.noreply.github.com>
2023-01-06 13:20:29 +0530

    Update john.sh

--- john.sh
+++ john.sh
@@ -9,7 +9,7 @@ RED="$(printf '\033[31m')"
 
 pkg update -y
 pkg upgrade -y
-pkg install figlet -y
+
 clear
 echo "${GREEN}--------------------------------------------------"
 echo "                    the 44 exploit                        "
@@ -21,17 +21,17 @@ echo
 echo
 echo "${GREEN}It will take 10-20 minutes..."
 echo
-echo "${GREEN}Watch my YouTube videos till Install"
+echo "${GREEN}Watch my YouTube videos till Install :)"
 
 sleep 15
 pkg install git make perl python binutils -y
 
 clear
-echo "${GREEN}--------------------------------------------------"
+echo "${GREEN}++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "                    the 44 exploit                        "
 echo "YouTube   https://www.youtube.com/@the44exploit           "
 echo "GitHub    https://github.com/the44exploit                 "
-echo "${GREEN}--------------------------------------------------"
+echo "${GREEN}++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo
 echo
 echo "$Green cloning repository"
@@ -42,11 +42,11 @@ git clone https://github.com/openwall/john
 
 cd john/src
 clear
-echo "${GREEN}--------------------------------------------------"
+echo "${GREEN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "                    the 44 exploit                        "
 echo "YouTube   https://www.youtube.com/@the44exploit           "
 echo "GitHub    https://github.com/the44exploit                 "
-echo "${GREEN}--------------------------------------------------"
+echo "${GREEN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo
 echo
 echo "$Green It will take 10-15 minutes..."
@@ -57,13 +57,18 @@ make -s clean && make -sj4
 cd ~/
 
 clear
-
-figlet the44exploit
-
+echo "${GREEN}the44exploit the44exploit the44exploit the44exploit"
+echo
+echo "                     the 44 exploit                        "
+echo
+echo "${GREEN}the44exploit the44exploit the44exploit the44exploit"
+echo
+echo
 echo "$GREEN YouTube   https://www.youtube.com/@the44exploit"
 echo "$GREEN GitHub    https://github.com/the44exploit"
-
-echo "$GREEN Succesfully Installed"
+echo
+echo
+echo "$GREEN Successfully Installed"
 
 #The 44 Exploit
 #YouTube https://www.youtube.com/@the44exploit
